ea805983a0be8a0e724f6945a6387f453185da79
Derek <derekmiracledavid@hotmail.com>
2021-08-17 15:42:58 +0100

    Initialised github repo

---
+++ name.cc
@@ -0,0 +1,11 @@
+// A program that outputs my name to the console
+// It is written in C++
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Derek David" << endl;
+    return 0;
+}
