763e0c6e28c932416a62011c7e9206d0f1b1305b
derek-dv <derekmiracledavid@hotmail.com>
2021-08-17 22:35:55 +0100

    Complete heroku setup

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ app.config['MAIL_PORT'] = 2525
 app.config['MAIL_USERNAME'] = '46450e11e28426'
 app.config['MAIL_PASSWORD'] = '72fb3830a310d4'
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['SECRET_KEY'] = 'the random string'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
